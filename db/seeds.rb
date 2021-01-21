ad5512cc31a207fcf49ba2dfef5fc1897ed1ec32
JasonAkiraJordan <jasonajordan0@gmail.com>
2021-01-21 18:52:15 -0500

    testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+#TESTING
 Event.destroy_all 
 Ticket.destroy_all
 User.destroy_all
