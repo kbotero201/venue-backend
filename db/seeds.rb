f7ef1147eb97a76934940355b26cfa7b28b32d64
JasonAkiraJordan <jasonajordan0@gmail.com>
2021-01-26 13:35:36 -0500

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,15 +19,17 @@ imgPlaceHolder = "https://via.placeholder.com/150"
 kevin = User.create(name: "Kevin", favorite: "Concerts", password: "123")
 jason = User.create(name: "Jason", favorite: "Concerts", password: "123")
 
-msg = Venue.create(name: "Madison Square Garden", location: "manhattan", description: "The World's Most Famous Arena", image: imgPlaceHolder) 
+msg = Venue.create(name: "Madison Square Garden", location: "manhattan", description: "The World's Most Famous Arena", image: "../images/msg.jpg") 
+flatiron = Venue.create(name: "Flatiron Campus", location: "manhattan", description: "The Best Bootcamp", image: "../images/flatiron.jpeg") 
 
-yeezy = Event.create(event_name: "Yeezy is Back", performer_name: "Kanye West", event_type: "Concert", description: "Who knows...", image: "./images/kanyewest.jpg", venue_id: msg.id)
-postponed = Event.create(event_name: "Where do we Go?", performer_name: "Billie Eilish", event_type: "Concert", description: "Appreantly this was postponed", image: imgPlaceHolder, venue_id: msg.id)
+
+yeezy = Event.create(event_name: "Yeezy is Back", performer_name: "Kanye West", event_type: "Concert", description: "Who knows...", image: "../images/kanyewest.jpg", venue_id: msg.id)
+postponed = Event.create(event_name: "Where do we Go?", performer_name: "Billie Eilish", event_type: "Concert", description: "Appreantly this was postponed", image: "../images/billie.jpg", venue_id: msg.id)
+studentTeacherShowdown = Event.create(event_name: "Flatiron Brawl 2021", performer_name: "Lazzzer Ian", event_type: "Party", description: "Ian fists are rated E for Everyone", image: "../images/Ian1.png", venue_id: flatiron.id)
+
 
 
 ticket1 = Ticket.create(user_id: kevin.id, event_id: yeezy.id)
-
-
 
 
 #tickkets
